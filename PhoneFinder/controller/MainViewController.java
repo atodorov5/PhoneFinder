6d14d5d9f5112011a94fd4ae76c89320f960ece4
Viktor <gameoverpy123@gmail.com>
2020-04-21 17:40:40 +0300

    features

--- PhoneFinder/controller/MainViewController.java
+++ PhoneFinder/controller/MainViewController.java
@@ -5,13 +5,23 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import model.Brand;
+import model.Camera;
+import model.Processor;
+import model.Type;
 
 public class MainViewController {
 	@FXML private AnchorPane newCarap= new AnchorPane();
 	@FXML private BorderPane mainBP;
 	@FXML private AnchorPane Home;
+	@FXML private ComboBox<Brand> cbBrand;
+	@FXML private ComboBox<Camera> cbCamera;
+	@FXML private ComboBox<Type> cbType;
+	@FXML private ComboBox<Processor> cbProcessor;
+	
 	
 	@FXML    public void addSpecsMenuItem() throws IOException {
         newCarap.getChildren().clear();
@@ -28,5 +38,6 @@ public class MainViewController {
         newCarap.getChildren().add((Node) FXMLLoader.load(getClass().getResource("/controller/AddPhoneView.fxml")));
         mainBP.setCenter(newCarap);
     }
+	
 
 }
